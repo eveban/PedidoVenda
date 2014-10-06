688250f657465465bc2c172deaf612d986a51366
Everson Silva <everson_gsilva@yahoo.com.br>
2014-10-06 11:22:46 -0300

    Inserindo gráficos

--- src/main/java/br/com/pedidovenda/repository/PedidoRepository.java
+++ src/main/java/br/com/pedidovenda/repository/PedidoRepository.java
@@ -1,19 +1,31 @@
 package br.com.pedidovenda.repository;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.transform.Transformers;
+import org.hibernate.type.StandardBasicTypes;
+import org.hibernate.type.Type;
 
 import br.com.pedidovenda.model.Pedido;
+import br.com.pedidovenda.model.Usuario;
+import br.com.pedidovenda.model.vo.DataValor;
 import br.com.pedidovenda.repository.filter.PedidoFilter;
 
 public class PedidoRepository implements Serializable {
@@ -24,48 +36,129 @@ public class PedidoRepository implements Serializable {
 	private EntityManager manager;
 
 	@SuppressWarnings("unchecked")
+	public Map<Date, BigDecimal> valoresTotaisPorData(Integer numeroDias,
+			Usuario criadoPor) {
+		// pegar numero de dias correto, pois o numeroDias aparece sempre com um
+		// dia a mais.
+		numeroDias -= 1;
+
+		Session session = manager.unwrap(Session.class);
+
+		// Data Inicial = Hoje
+		Calendar dataInicial = Calendar.getInstance();
+		// Truncado a data para não pegar HH:mm:ss
+		dataInicial = DateUtils.truncate(dataInicial, Calendar.DAY_OF_MONTH);
+		/*
+		 * adicionando um valor negativo para pegar data retroativa para
+		 * montagem do gráfico. Transformando a numeroDias em valor negativo Ex:
+		 * Data atual - numeroDias
+		 */
+		dataInicial.add(Calendar.DAY_OF_MONTH, numeroDias * -1);
+
+		// Criando um Mapa vazio
+		Map<Date, BigDecimal> resultado = criarMapaVazio(numeroDias,
+				dataInicial);
+
+		Criteria criteria = session.createCriteria(Pedido.class);
+
+		// Para utilizar o Sum ou Max - usamos Projeção - Ex: select
+		// date(data_criacao) as data, sum(valor_total) as valor from pedido
+		// where... group by date(data_criacao)
+		criteria.setProjection(
+				Projections
+						.projectionList()
+						.add(Projections.sqlGroupProjection(
+								"date(data_criacao) as data",
+								"date(data_criacao)", new String[] { "data" },
+								new Type[] { StandardBasicTypes.DATE }))
+						.add(Projections.sum("valorTotal").as("valor"))).add(
+				Restrictions.ge("dataCriacao", dataInicial.getTime()));
+
+		if (criadoPor != null) {
+			criteria.add(Restrictions.eq("vendedor", criadoPor));
+		}
+		List<DataValor> valoresPorData = criteria.setResultTransformer(
+				Transformers.aliasToBean(DataValor.class)).list();
+
+		for (DataValor dataValor : valoresPorData) {
+			resultado.put(dataValor.getData(), dataValor.getValor());
+
+		}
+
+		return resultado;
+
+	}
+
+	// Se não houve pedido no determinado dia, a linha do gráfico precisa
+	// descer no zero
+	private Map<Date, BigDecimal> criarMapaVazio(Integer numeroDias,
+			Calendar dataInicial) {
+
+		// Clonando, senão a dataInciail.add não retorna nada
+		dataInicial = (Calendar) dataInicial.clone();
+		Map<Date, BigDecimal> mapaInicial = new TreeMap<>();
+
+		for (int i = 0; i <= numeroDias; i++) {
+			mapaInicial.put(dataInicial.getTime(), BigDecimal.ZERO);
+			dataInicial.add(Calendar.DAY_OF_MONTH, 1);
+		}
+
+		return mapaInicial;
+	}
+
+	@SuppressWarnings("unchecked")
 	public List<Pedido> filtrados(PedidoFilter filtro) {
 		Session session = this.manager.unwrap(Session.class);
-		
+
 		Criteria criteria = session.createCriteria(Pedido.class)
-				// fazemos uma associação (join) com cliente e nomeamos como "c"
+		// fazemos uma associação (join) com cliente e nomeamos como "c"
 				.createAlias("cliente", "c")
-				// fazemos uma associação (join) com vendedor e nomeamos como "v"
+				// fazemos uma associação (join) com vendedor e nomeamos como
+				// "v"
 				.createAlias("vendedor", "v");
-		
+
 		if (filtro.getNumeroDe() != null) {
-			// id deve ser maior ou igual (ge = greater or equals) a filtro.numeroDe
+			// id deve ser maior ou igual (ge = greater or equals) a
+			// filtro.numeroDe
 			criteria.add(Restrictions.ge("id", filtro.getNumeroDe()));
 		}
 
 		if (filtro.getNumeroAte() != null) {
-			// id deve ser menor ou igual (le = lower or equal) a filtro.numeroDe
+			// id deve ser menor ou igual (le = lower or equal) a
+			// filtro.numeroDe
 			criteria.add(Restrictions.le("id", filtro.getNumeroAte()));
 		}
 
 		if (filtro.getDataCriacaoDe() != null) {
-			criteria.add(Restrictions.ge("dataCriacao", filtro.getDataCriacaoDe()));
+			criteria.add(Restrictions.ge("dataCriacao",
+					filtro.getDataCriacaoDe()));
 		}
-		
+
 		if (filtro.getDataCriacaoAte() != null) {
-			criteria.add(Restrictions.le("dataCriacao", filtro.getDataCriacaoAte()));
+			criteria.add(Restrictions.le("dataCriacao",
+					filtro.getDataCriacaoAte()));
 		}
-		
+
 		if (StringUtils.isNotBlank(filtro.getNomeCliente())) {
-			// acessamos o nome do cliente associado ao pedido pelo alias "c", criado anteriormente
-			criteria.add(Restrictions.ilike("c.nome", filtro.getNomeCliente(), MatchMode.ANYWHERE));
+			// acessamos o nome do cliente associado ao pedido pelo alias "c",
+			// criado anteriormente
+			criteria.add(Restrictions.ilike("c.nome", filtro.getNomeCliente(),
+					MatchMode.ANYWHERE));
 		}
-		
+
 		if (StringUtils.isNotBlank(filtro.getNomeVendedor())) {
-			// acessamos o nome do vendedor associado ao pedido pelo alias "v", criado anteriormente
-			criteria.add(Restrictions.ilike("v.nome", filtro.getNomeVendedor(), MatchMode.ANYWHERE));
+			// acessamos o nome do vendedor associado ao pedido pelo alias "v",
+			// criado anteriormente
+			criteria.add(Restrictions.ilike("v.nome", filtro.getNomeVendedor(),
+					MatchMode.ANYWHERE));
 		}
-		
+
 		if (filtro.getStatuses() != null && filtro.getStatuses().length > 0) {
-			// adicionamos uma restrição "in", passando um array de constantes da enum StatusPedido
+			// adicionamos uma restrição "in", passando um array de constantes
+			// da enum StatusPedido
 			criteria.add(Restrictions.in("status", filtro.getStatuses()));
 		}
-		
+
 		return criteria.addOrder(Order.asc("id")).list();
 	}
 
@@ -76,5 +169,5 @@ public class PedidoRepository implements Serializable {
 	public Pedido porId(Long id) {
 		return this.manager.find(Pedido.class, id);
 	}
-	
+
 }
--- src/main/java/br/com/pedidovenda/security/Seguranca.java
+++ src/main/java/br/com/pedidovenda/security/Seguranca.java
@@ -1,6 +1,7 @@
 package br.com.pedidovenda.security;
 
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Produces;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
@@ -14,40 +15,42 @@ public class Seguranca {
 
 	@Inject
 	private ExternalContext externalContext;
-	
+
 	public String getNomeUsuario() {
 		String nome = null;
-		
+
 		UsuarioSistema usuarioLogado = getUsuarioLogado();
-		
+
 		if (usuarioLogado != null) {
 			nome = usuarioLogado.getUsuario().getNome();
 		}
-		
+
 		return nome;
 	}
 
-	private UsuarioSistema getUsuarioLogado() {
+	@Produces
+	@UsuarioLogado
+	public UsuarioSistema getUsuarioLogado() {
 		UsuarioSistema usuario = null;
-		
-		UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) 
-				FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
-		
+
+		UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) FacesContext
+				.getCurrentInstance().getExternalContext().getUserPrincipal();
+
 		if (auth != null && auth.getPrincipal() != null) {
 			usuario = (UsuarioSistema) auth.getPrincipal();
 		}
-		
+
 		return usuario;
 	}
-	
+
 	public boolean isEmitirPedidoPermitido() {
-		return externalContext.isUserInRole("ADMINISTRADORES") 
+		return externalContext.isUserInRole("ADMINISTRADORES")
 				|| externalContext.isUserInRole("VENDEDORES");
 	}
-	
+
 	public boolean isCancelarPedidoPermitido() {
-		return externalContext.isUserInRole("ADMINISTRADORES") 
+		return externalContext.isUserInRole("ADMINISTRADORES")
 				|| externalContext.isUserInRole("VENDEDORES");
 	}
-	
+
 }
